fe94e5f93e960e2c1ff04fcc97116afadae7bdaf
Pavithrakadali <kpavithras99@gmail.com>
2023-09-25 14:21:56 +0530

    Evennumbers

---
+++ JavaProjectExample/src/Inheritance/AnswerDemo.java
@@ -0,0 +1,19 @@
+package Inheritance;
+class Demo9
+{
+	void display()
+	{
+		System.out.println(" EVen no");
+	}
+}
+
+public class AnswerDemo {
+
+	public static void main(String[] args) {
+		Demo2 obj =new Demo2();
+		obj.display();
+				
+	}
+
+}
+
